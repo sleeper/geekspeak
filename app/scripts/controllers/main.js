5295449344959af91bdff9e620f040f2c49502f4
Frederick Ros <frederick.ros@gmail.com>
2012-10-24 17:52:37 +0200

    Votes are now working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 geekspeakApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
-  
+
   $scope.activeTalks = [
-  	{title: "The fantastic history of Linux", presenter: "Torvalds"},
-  	{title: "Is this the real thing ?", presenter: "Madonna"}
+  	{title: "The fantastic history of Linux", presenter: "Torvalds", votes: 10},
+  	{title: "The fantastic history of vim", presenter: "Who Knows ? ", votes: 15},
+  	{title: "Is this the real thing ?", presenter: "Madonna", votes: 50}
   ];
+
+  $scope.increaseVotes = function(t) { t.votes += 1; }
+
+
 });
