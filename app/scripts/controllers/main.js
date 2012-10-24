c89a2143983822544407aaba03133a26f824d301
Frederick Ros <frederick.ros@gmail.com>
2012-10-24 23:30:57 +0200

    WIP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,12 +3,38 @@
 geekspeakApp.controller('MainCtrl', function($scope) {
 
   $scope.activeTalks = [
-  	{title: "The fantastic history of Linux", presenter: "Torvalds", votes: 10},
-  	{title: "The fantastic history of vim", presenter: "Who Knows ? ", votes: 15},
-  	{title: "Is this the real thing ?", presenter: "Madonna", votes: 50}
+  	{
+  		title: "The fantastic history of Linux", 
+  		presenter: "Torvalds",
+  		proposed_by: "fred",
+  		proposed_on: "2012-10-24 22:03:52",
+  		comments: "This should be a fantastic history with a lot of bad w=mouth words ...",
+  		votes: 10
+  	},
+  	{
+  		title: "The fantastic history of vim",
+  		presenter: "Who Knows ? ",
+  		proposed_by: "julien",
+  		proposed_on: "2012-09-22 14:03:52",
+  		comments: "Well ... cryptic, mostly ;)",
+  		votes: 15
+  	},
+  	{
+  		title: "Is this the real thing ?", 
+  		presenter: "Madonna", 
+  		proposed_by: "pascal",
+  		proposed_on: "2012-10-22 09:14:21",
+  		comments: "Yes, lots of songs, dancing boys and girls, ...",
+  		votes: 50
+  	}
   ];
 
-  $scope.increaseVotes = function(t) { t.votes += 1; }
+  $scope.isDisabled = false;
+
+  $scope.increaseVotes = function(t) { 
+  	t.votes += 1; 
+  	t.voted = true;
+  }
 
 
 });
