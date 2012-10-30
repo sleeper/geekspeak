798e17a5fc813755c290b6cc27f085863265778c
Frederick Ros <frederick.ros@gmail.com>
2012-10-30 23:55:02 +0100

    WIP

--- app/scripts/directives/talk.js
+++ app/scripts/directives/talk.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// A talk has several attributes:
+// * title
+// * presenter
+// * creation date
+// * created by
+// * votes
+// * comments
+// * presentation date
+//
 geekspeakApp.directive('talk', function() {
   return {
     templateUrl: 'views/talk.html',
